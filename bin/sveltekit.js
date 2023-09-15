2717fc419badfc9f90524ffcc70cba534c2f4bb8
PMolnes <pettermolnes@icloud.com>
2023-09-15 17:38:34 +0200

    update: using copyFile instead of execSync

--- bin/sveltekit.js
+++ bin/sveltekit.js
@@ -4,16 +4,11 @@ export default function setupSvelteKit(packageManager, projectName) {
 	console.log("Setup your SvelteKit project...");
 	helper.executeCommand(`${packageManager} create svelte@latest ${projectName}`);
 
+	console.log("Initializing tailwindcss...");
 	helper.initializeTailwindCSS(packageManager, projectName);
 
-	helper.executeCommand(
-		`cd ${projectName} && ${helper.copyTemplateFileString(
-			"sveltekit",
-			"tailwind.config.js"
-		)} && ${helper.copyTemplateFileString("sveltekit", "app.css", "./src")} && ${helper.copyTemplateFileString(
-			"sveltekit",
-			"+layout.svelte",
-			"./src/routes"
-		)} && ${helper.copyTemplateFileString("sveltekit", "svelte.config.js")}`
-	);
+	helper.copyFile("sveltekit", "tailwind.config.js", projectName, ["tailwind.config.js"]);
+	helper.copyFile("sveltekit", "svelte.config.js", projectName, ["svelte.config.js"]);
+	helper.copyFile("sveltekit", "app.css", projectName, ["src", "app.css"]);
+	helper.copyFile("sveltekit", "+layout.svelte", projectName, ["src", "routes", "+layout.svelte"]);
 }
