2126020f6e3b860d23e829990e1425497a324d88
Joakim Edvardsen <77448888+jKm00@users.noreply.github.com>
2024-05-22 21:12:55 +0200

    wip: testing

--- bin/test/initializer.test.js
+++ bin/test/initializer.test.js
@@ -13,6 +13,9 @@ import { copyTemplateFiles } from '../sveltekit.js';
 import { readdirSync, rmSync } from 'fs';
 import { create } from 'create-svelte';
 import { TEMPLATES } from '../templates/sveltekit/definitions.js';
+// TODO: Test cli tool
+// import execa from 'execa';
+// import stipAnsi from 'strip-ansi';
 
 function setupProjectInfo(framework, language, packageManager, projectName) {
 	setFramework(framework);
@@ -25,7 +28,8 @@ afterEach(() => {
 	rmSync(helper.getProjectPath(), { recursive: true, force: true });
 });
 
-test('vue.js successful setup', () => {
+describe('vue', () => {
+	test('vue.js successful setup', () => {
 	// setup
 	setupProjectInfo('vue', 'js', 'pnpm', 'my-test-app');
 	setupVue();
@@ -52,8 +56,10 @@ test('vue.ts successful setup', () => {
 	expect(srcFiles.includes('style.css')).toBeTruthy();
 	expect(srcFiles.includes('main.ts')).toBeTruthy();
 });
+})
 
-test('react.js successful setup', () => {
+describe('react', () => {
+	test('react.js successful setup', () => {
 	setupProjectInfo('react', 'ts', 'yarn', 'my-test-app');
 	setupReact();
 
@@ -65,8 +71,14 @@ test('react.js successful setup', () => {
 	expect(srcFiles.includes('App.tsx')).toBeTruthy();
 	expect(srcFiles.includes('index.css')).toBeTruthy();
 });
+})
 
-test('sveltekit basic successful setup', async () => {
+describe('sveltekit', () => {
+	it('should contain two templates', async () => {
+		expect(TEMPLATES.length).toBe(2);
+	})
+
+	test('sveltekit basic successful setup', async () => {
 	setupProjectInfo('sveltekit', '', '', 'my-test-app');
 	await create(getProjectInfo().projectName, {
 		name: getProjectInfo().projectName,
@@ -88,3 +100,40 @@ test('sveltekit basic successful setup', async () => {
 	expect(srcFiles.includes('app.css')).toBeTruthy();
 	expect(routes.includes('+layout.svelte')).toBeTruthy();
 });
+
+	// TODO: Uncomment and continue when cli testing is ready
+	// it('should create basic sveltekit project', async () => {
+	// 	const cliPath = path.join(__dirname, '../index.js');
+
+	// 	const inputs = [
+	// 		'1\n', // sveltekit
+	// 		'3\n', // pnpm
+	// 		'my-test-app\n', // project name
+	// 		'2\n', // JSMKit
+	// 		'2\n', // Skeleton project
+	// 		'1\n', // Yes, using TypeScript syntax
+	// 		'\u001B[B', // Arrow down
+	// 		'\x20', // Prettier
+	// 		'\u001B[B', // Arrow down
+	// 		'\x20', // Playwright
+	// 		'\u001B[B', // Arrow down
+	// 		'\x20', // Vitest
+	// 		'\x0D', // Enter
+	// 	]
+	// 	const { stdout } = await execa('node', [cliPath], {
+	// 		input: inputs.join(''),
+	// 	});
+
+	// 	const cleanOutput = stripAnsi(stdout);
+
+	// 	expect(cleanOutput).toContain('sveltekit');
+	// 	expect(cleanOutput).toContain('pnpm');
+	// 	expect(cleanOutput).toContain('my-test-app');
+	// 	expect(cleanOutput).toContain('JKMKit');
+	// 	expect(cleanOutput).toContain('Skeleton project');
+	// 	expect(cleanOutput).toContain('Yes, using TypeScript syntax');
+	// 	expect(cleanOutput).toContain('Prettier');
+	// 	expect(cleanOutput).toContain('Playwright');
+	// 	expect(cleanOutput).toContain('Vitest');
+	// })
+})
